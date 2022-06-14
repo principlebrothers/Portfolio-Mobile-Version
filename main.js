bb0ca0b58d851db92531c2c57915be422b5160b2
principlebrothers <principlesbrothers@yahoo.com>
2022-06-14 10:11:50 +0000

    Fix page formatting error

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const menuButton = document.querySelector('#metaMenu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const closedButton = document.querySelector('.closed-btn'); 
-const menuLinks = document.querySelectorAll('.in-text-link'); 
+const closedButton = document.querySelector('.closed-btn');
+const menuLinks = document.querySelectorAll('.in-text-link');
 
-menuButton.addEventListener('click', () => { 
-    mobileMenu.style.display = 'block'; }) 
+menuButton.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
+})
+
+closedButton.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+})
+
+for (let i = 0; i < menuLinks.length; i += 1) {
+  menuLinks[i].addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+  })
+}
